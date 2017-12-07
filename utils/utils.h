cb2c6fb507b4220273929d63d4f827e59488ae55
Jorge Sepulveda <jorge.a.sepulveda@gmail.com>
2017-12-07 12:09:10 -0300

    Modifications of the struct

--- utils/utils.h
+++ utils/utils.h
@@ -11,6 +11,68 @@
 using namespace cv;
 
 
+
+typedef struct rectangle_type {
+
+  rectangle_type():rect(0,0,0,0),id(0),prob(0){};
+  rectangle_type(Rect r, int i, double p):rect(r),id(i),prob(p){};
+
+  //copy constructor
+  rectangle_type (const rectangle_type & rhs) { *this = rhs; };
+
+  //Inequality operator
+  bool operator !=(const rectangle_type &rhs) const
+  {
+    return ((rect!=rhs.rect)||(id!=rhs.id)||(prob!=rhs.prob));
+  };
+
+  //Inequality operator
+  bool operator ==(const rectangle_type &rhs) const
+  {
+    return ((rect==rhs.rect)&&(id==rhs.id)&&(prob==rhs.prob));
+  };
+
+  // assigment operator
+  rectangle_type &operator =(const rectangle_type &rhs)
+  {
+      if (*this != rhs)
+      {
+          rect=rhs.rect; id = rhs.id; prob = rhs.prob;
+      }
+      return *this;
+  };
+
+  // overloaded += operator
+  rectangle_type & operator +=(const rectangle_type  &rhs)
+  {
+    // union of rects.
+    Rect u_rect   = rect | rhs.rect ;
+    // save bigger probability.
+    double u_prob = (prob>rhs.prob ? prob : rhs.prob);
+    rect = u_rect;
+    prob = u_prob;
+    return *this;
+  };
+
+  const rectangle_type operator+(const rectangle_type &rhs) const
+  {
+     rectangle_type result(*this);
+     return result += rhs;
+  }
+
+  Rect   rect;
+  int    id;
+  double prob;
+
+} rectangle_t;
+
+
+bool rectangle_sorter (rectangle_type const& lhs, rectangle_type const& rhs) {
+    return lhs.prob > rhs.prob;
+}
+
+
+
 typedef struct class_struct {
 
   class_struct():id(0),prob(0){};
@@ -45,8 +107,16 @@ typedef struct prob_type {
   //Inequality operator
   bool operator !=(const prob_type &rhs) const
   {
-      return ((rect!=rhs.rect)||(Id!=rhs.Id)||(Prob!=rhs.Prob));
+    return ((rect!=rhs.rect)||(Id!=rhs.Id)||(Prob!=rhs.Prob));
   };
+
+  //Inequality operator
+  bool operator ==(const prob_type &rhs) const
+  {
+    return ((rect==rhs.rect)&&(Id==rhs.Id)&&(Prob==rhs.Prob));
+  };
+
+
   // assigment operator
   prob_type &operator =(const prob_type &rhs)
   {
@@ -62,9 +132,6 @@ typedef struct prob_type {
   double Prob;
 
 } prob_t;
-
-
-
 typedef struct rect_struct {
   int x;
   int y;
